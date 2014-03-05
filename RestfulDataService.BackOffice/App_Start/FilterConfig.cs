5ffecef1d1bc295859683c189465697efba38da9
Ravi <Ravi@c8ee6280-496e-bb49-91e5-1ffda20e84f4>
2014-03-05 15:44:43 +0000

    add initial test projects structure

---
+++ RestfulDataService.BackOffice/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace RestfulDataService.BackOffice
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
