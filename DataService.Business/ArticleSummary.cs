ca24193a3b2be30fe5f441b944c58f3710c8c2bc
Ravi <Ravi@c8ee6280-496e-bb49-91e5-1ffda20e84f4>
2014-04-02 14:33:15 +0000

    missing type from from previous commit added

---
+++ DataService.Business/ArticleSummary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twg.chk.DataService.Business
+{
+    public class ArticleSummary: IIdentification
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public String Introduction { get; set; }
+        public DateTime LastModified { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public MediaContent ThumbnailImage { get; set; }
+        public Person Author { get; set; }
+
+        public dynamic GetIdentityItem()
+        {
+            return Id;
+        }
+    }
+}
