e371f7125542ed581899fe8da92418a3f39cd351
Ravi <Ravi@c8ee6280-496e-bb49-91e5-1ffda20e84f4>
2014-04-24 16:58:41 +0000

    moved default user and roles seed inside the main migration Configuration class

--- DataService.DbContext/Migrations/Configuration.cs
+++ DataService.DbContext/Migrations/Configuration.cs
@@ -4,6 +4,9 @@ namespace twg.chk.DataService.DbContext.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Diagnostics;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
 
     public class Configuration : DbMigrationsConfiguration<twg.chk.DataService.DbContext.DataServiceEntities>
     {
@@ -15,18 +18,63 @@ namespace twg.chk.DataService.DbContext.Migrations
 
         protected override void Seed(twg.chk.DataService.DbContext.DataServiceEntities context)
         {
-            //  This method will be called after migrating to the latest version.
+            var backOfficeGroup = context.Roles.SingleOrDefault(r => r.Name.Equals("backofficegroup"));
+            if (backOfficeGroup == null)
+            {
+                backOfficeGroup = new IdentityRole { Name = "backofficegroup" };
+                context.Roles.Add(backOfficeGroup);
+                context.Commit();
+                Debug.WriteLine("Created backofficegroup role");
+            }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            var frontofficegroup = context.Roles.SingleOrDefault(r => r.Name.Equals("frontofficegroup"));
+            if (frontofficegroup == null)
+            {
+                frontofficegroup = new IdentityRole { Name = "frontofficegroup" };
+                context.Roles.Add(frontofficegroup);
+                context.Commit();
+                Debug.WriteLine("Created frontofficegroup role");
+            }
+
+            var user = context.Users.SingleOrDefault(u => u.UserName.Equals("backofficeadmin"));
+            if (user == null)
+            {
+                user = new IdentityUser
+                {
+                    UserName = "backofficeadmin",
+                    PasswordHash = new PasswordHasher().HashPassword("Passw0rd"),
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+                context.Users.Add(user);
+                context.Commit();
+                Debug.WriteLine("Created backofficeadmin user");
+
+                user.Roles.Add(new IdentityUserRole { UserId = user.Id, RoleId = backOfficeGroup.Id });
+                context.Commit();
+                Debug.WriteLine("Added to backofficegroup role backofficeadmin user");
+
+                user.Roles.Add(new IdentityUserRole { UserId = user.Id, RoleId = frontofficegroup.Id });
+                context.Commit();
+                Debug.WriteLine("Added to frontofficegroup role backofficeadmin user");
+            }
+
+            var user2 = context.Users.SingleOrDefault(u => u.UserName.Equals("frontofficeuser"));
+            if (user2 == null)
+            {
+                user2 = new IdentityUser
+                {
+                    UserName = "frontofficeuser",
+                    PasswordHash = new PasswordHasher().HashPassword("Passw0rd"),
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+                context.Users.Add(user2);
+                context.Commit();
+                Debug.WriteLine("Created frontofficeuser user");
+
+                user2.Roles.Add(new IdentityUserRole { UserId = user2.Id, RoleId = frontofficegroup.Id });
+                context.Commit();
+                Debug.WriteLine("Added to frontofficegroup role frontofficeuser user");
+            }
         }
     }
 }
--- DataService.DbContext/Migrations/DataServiceSampleData.cs
+++ DataService.DbContext/Migrations/DataServiceSampleData.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Data.Entity;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using System.Diagnostics;
 using System.Data.Entity.Migrations;
 
 using twg.chk.DataService.Business;
@@ -22,64 +19,6 @@ namespace twg.chk.DataService.DbContext.Migrations
         protected override void Seed(DataServiceEntities context)
         {
             base.Seed(context);
-
-            var backOfficeGroup = context.Roles.SingleOrDefault(r => r.Name.Equals("backofficegroup"));
-            if (backOfficeGroup == null)
-            {
-                backOfficeGroup = new IdentityRole { Name = "backofficegroup" };
-                context.Roles.Add(backOfficeGroup);
-                context.Commit();
-                Debug.WriteLine("Created backofficegroup role");
-            }
-
-            var frontofficegroup = context.Roles.SingleOrDefault(r => r.Name.Equals("frontofficegroup"));
-            if (frontofficegroup == null)
-            {
-                frontofficegroup = new IdentityRole { Name = "frontofficegroup" };
-                context.Roles.Add(frontofficegroup);
-                context.Commit();
-                Debug.WriteLine("Created frontofficegroup role");
-            }
-
-            var user = context.Users.SingleOrDefault(u => u.UserName.Equals("backofficeadmin"));
-            if (user == null)
-            {
-                user = new IdentityUser
-                {
-                    UserName = "backofficeadmin",
-                    PasswordHash = new PasswordHasher().HashPassword("Passw0rd"),
-                    SecurityStamp = Guid.NewGuid().ToString()
-                };
-                context.Users.Add(user);
-                context.Commit();
-                Debug.WriteLine("Created backofficeadmin user");
-
-                user.Roles.Add(new IdentityUserRole { UserId = user.Id, RoleId = backOfficeGroup.Id });
-                context.Commit();
-                Debug.WriteLine("Added to backofficegroup role backofficeadmin user");
-
-                user.Roles.Add(new IdentityUserRole { UserId = user.Id, RoleId = frontofficegroup.Id });
-                context.Commit();
-                Debug.WriteLine("Added to frontofficegroup role backofficeadmin user");
-            }
-
-            var user2 = context.Users.SingleOrDefault(u => u.UserName.Equals("frontofficeuser"));
-            if (user2 == null)
-            {
-                user2 = new IdentityUser
-                {
-                    UserName = "frontofficeuser",
-                    PasswordHash = new PasswordHasher().HashPassword("Passw0rd"),
-                    SecurityStamp = Guid.NewGuid().ToString()
-                };
-                context.Users.Add(user2);
-                context.Commit();
-                Debug.WriteLine("Created frontofficeuser user");
-
-                user2.Roles.Add(new IdentityUserRole { UserId = user2.Id, RoleId = frontofficegroup.Id });
-                context.Commit();
-                Debug.WriteLine("Added to frontofficegroup role frontofficeuser user");
-            }
 
             // Seed static content link **************************************************************
             var disclaimer = context.StaticContentLinks.SingleOrDefault(s => s.IdentificationValue.Equals("disclaimer"));
