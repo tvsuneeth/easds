bdef54186fd197a6dbcb56c9da074b0e4060d8d0
Suneeth <Suneeth@c8ee6280-496e-bb49-91e5-1ffda20e84f4>
2014-06-05 10:54:03 +0000

    updating GetArticleTaxonomies stored proc to include data from singlecategoryitem assignments DB table

--- chk.dal/DatabaseScripts/chk.GetArticleTaxonomies.sql
+++ chk.dal/DatabaseScripts/chk.GetArticleTaxonomies.sql
@@ -1,6 +1,6 @@
 USE [CatererAndHotelKeeper_systest]
 GO
-/****** Object:  StoredProcedure [chk].[GetArticleTaxonomies]    Script Date: 05/30/2014 11:52:35 ******/
+/****** Object:  StoredProcedure [chk].[GetArticleTaxonomies]    Script Date: 06/05/2014 11:23:47 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -19,16 +19,23 @@ BEGIN
 		select ci.liCategoryID from ArticleMultipleCategoryItemAssignments  amc 
 		inner join CategoryItems ci on ci.liCategoryItemID = amc.liCategoryItemID	    
 		where amc.liArticleID = @ArticleId AND (ci.bRemoved IS NULL OR ci.bRemoved = 0)
+		UNION
+		select ci.liCategoryID from ArticleSingleCategoryItemAssignments  asca 
+		inner join CategoryItems ci on ci.liCategoryItemID = asca.liCategoryItemID    
+		where asca.liArticleID = @ArticleId AND (ci.bRemoved IS NULL OR ci.bRemoved = 0)
 	)
 	
 	select ci.liCategoryID, ci.liCategoryItemID,ci.sItemName, ci.liParentID from ArticleMultipleCategoryItemAssignments  amc 
 	inner join CategoryItems ci on ci.liCategoryItemID = amc.liCategoryItemID	
 	where amc.liArticleID = @ArticleId AND (ci.bRemoved IS NULL OR ci.bRemoved = 0)
+	UNION
+	select ci.liCategoryID, ci.liCategoryItemID,ci.sItemName, ci.liParentID from ArticleSingleCategoryItemAssignments  asca 
+	inner join CategoryItems ci on ci.liCategoryItemID = asca.liCategoryItemID	
+	where asca.liArticleID = @ArticleId AND (ci.bRemoved IS NULL OR ci.bRemoved = 0)
 	
-	
-	/*select ci.liCategoryItemID,ci.sItemName, c.liCategoryID,c.sCategoryName, ci.liParentID from ArticleMultipleCategoryItemAssignments  amc 
-	inner join CategoryItems ci on ci.liCategoryItemID = amc.liCategoryItemID
-	inner join Categories c on c.liCategoryID = ci.liCategoryID
-	where amc.liArticleID = @ArticleId AND (ci.bRemoved IS NULL OR ci.bRemoved = 0)*/
 	
 END
+
+
+	
+
