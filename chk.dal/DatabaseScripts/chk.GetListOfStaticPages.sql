6e7cd9f3869f29efd77e09117c361e621c727898
Suneeth <Suneeth@c8ee6280-496e-bb49-91e5-1ffda20e84f4>
2014-06-25 08:41:04 +0000

    updating stored procedures..

--- chk.dal/DatabaseScripts/chk.GetListOfStaticPages.sql
+++ chk.dal/DatabaseScripts/chk.GetListOfStaticPages.sql
@@ -1,23 +1,23 @@
-﻿USE [CatererAndHotelKeeper_dev_latest]
+﻿USE [CatererAndHotelKeeper_systest]
 GO
-/****** Object:  StoredProcedure [chk].[GetStaticPage]    Script Date: 06/05/2014 14:04:29 ******/
+/****** Object:  StoredProcedure [chk].[GetListOfStaticPages]    Script Date: 06/25/2014 09:39:08 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
 
-CREATE PROCEDURE [chk].[GetListOfStaticPages]
+ALTER PROCEDURE [chk].[GetListOfStaticPages]
 
 AS
 BEGIN
-	SELECT 
-		liStaticPageID,
-		sPageURL
+	
 
-	FROM dbo.StaticPages
-	WHERE bLive = 1
-	ORDER BY liStaticPageID
+select sp.liStaticPageID , pub.sPageURL  from  StaticPages sp
+inner join StaticPages pub on sp.liLiveStaticPageID  = pub.liStaticPageID
+where sp.liLiveStaticPageID is not null and pub.bLive=1 and pub.liLiveStaticPageID is null
+order by sp.liStaticPageID
+
 END
 
 
--- chk.dal/DatabaseScripts/chk.GetStaticPage.sql
+++ chk.dal/DatabaseScripts/chk.GetStaticPage.sql
@@ -1,36 +1,49 @@
-USE [CatererAndHotelKeeper_dev]
+USE [CatererAndHotelKeeper_dev_latest]
 GO
-
-/****** Object:  StoredProcedure [chk].[GetStaticPage]    Script Date: 16/05/2014 17:26:10 ******/
+/****** Object:  StoredProcedure [chk].[GetStaticPage]    Script Date: 06/25/2014 09:37:44 ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
 
 
-CREATE PROCEDURE [chk].[GetStaticPage]
+ALTER PROCEDURE [chk].[GetStaticPage]
 (
 	@StaticPageId INT = -1,
 	@StaticPageName NVARCHAR(255) = NULL
 )
 AS
 BEGIN
-	SELECT TOP 1
-		  liStaticPageID
-		, sTitle
-		, sBody
-		, dtLastModified
-		, sMetaDescription
-		, sMetaKeywords
-		, sPageURL
-		, sPageTitle
-	FROM dbo.StaticPages
-	WHERE (@StaticPageId = -1 OR liStaticPageID = @StaticPageId)
-	AND (@StaticPageName IS NULL OR sPageURL LIKE @StaticPageName)
-	AND bLive = 1
-	ORDER BY dtLastModified DESC
+    
+ --   DECLARE @LiveStaticPageId INT
+    
+ --   Select @LiveStaticPageId 
+
+	--SELECT TOP 1
+	--	  liStaticPageID
+	--	, sTitle
+	--	, sBody
+	--	, dtLastModified
+	--	, sMetaDescription
+	--	, sMetaKeywords
+	--	, sPageURL
+	--	, sPageTitle
+	--FROM dbo.StaticPages
+	--WHERE (@StaticPageId = -1 OR liStaticPageID = @StaticPageId)
+	--AND (@StaticPageName IS NULL OR sPageURL LIKE @StaticPageName)
+	--AND bLive = 1
+	--ORDER BY dtLastModified DESC
+	
+	select sp.liStaticPageID 
+	, pub.sTitle
+	, pub.sBody
+	, pub.dtLastModified
+	, pub.sMetaDescription
+	, pub.sMetaKeywords
+	, pub.sPageURL
+	, pub.sPageTitle  from  StaticPages sp
+	inner join StaticPages pub on sp.liLiveStaticPageID  = pub.liStaticPageID
+	where 
+	(@StaticPageId = -1 OR sp.liStaticPageID = @StaticPageId)
+
 END
-GO
-
-
