f6b01656f00ccb73f9a8ecd73af90905d3c1f525
Suneeth <Suneeth@c8ee6280-496e-bb49-91e5-1ffda20e84f4>
2014-06-26 13:13:48 +0000

    enabling error handling

--- DataService.FrontOffice/Startup.cs
+++ DataService.FrontOffice/Startup.cs
@@ -6,6 +6,9 @@ using Microsoft.Owin;
 using Ninject;
 
 using twg.chk.DataService.DbContext.Migrations;
+using System.Configuration;
+using System.Web.Configuration;
+using System.Web.Http;
 
 [assembly: OwinStartup(typeof(twg.chk.DataService.FrontOffice.Startup))]
 namespace twg.chk.DataService.FrontOffice
@@ -21,8 +24,42 @@ namespace twg.chk.DataService.FrontOffice
         }
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            ConfigureAuth(app);         
             ConfigureWebApi(app);
+
+            ConfigureErrorHandling();
+
+           
+        }
+
+        private static void ConfigureErrorHandling()
+        {
+
+            var config = (CustomErrorsSection)
+      ConfigurationManager.GetSection("system.web/customErrors");
+
+            IncludeErrorDetailPolicy errorDetailPolicy;
+
+            switch (config.Mode)
+            {
+                case CustomErrorsMode.RemoteOnly:
+                    errorDetailPolicy
+                        = IncludeErrorDetailPolicy.LocalOnly;
+                    break;
+                case CustomErrorsMode.On:
+                    errorDetailPolicy
+                        = IncludeErrorDetailPolicy.Never;
+                    break;
+                case CustomErrorsMode.Off:
+                    errorDetailPolicy
+                        = IncludeErrorDetailPolicy.Always;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            GlobalConfiguration.Configuration.IncludeErrorDetailPolicy
+                = errorDetailPolicy;
         }
     }
 }
