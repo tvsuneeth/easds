3ea57e642131a97a84a058cc0ab887fe5149a994
Suneeth <Suneeth@c8ee6280-496e-bb49-91e5-1ffda20e84f4>
2014-09-10 11:33:21 +0000

    Updating Unit test assembly and file names

---
+++ TWG.EASDataService.Api.Tests/SlotControllerTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+
+using TWG.EASDataService.Business;
+using TWG.EASDataService.Data.Repository;
+using TWG.EASDataService.Services;
+using TWG.EASDataService.Api.Controllers;
+using TWG.EASDataService.Api.Models;
+using TWG.EASDataService.Api.Helpers;
+using System.Web.Http.Routing;
+using System.Web.Http.Hosting;
+
+namespace TWG.EASDataService.Api.Tests
+{
+    [TestClass]
+    public class SlotControllerTests
+    {
+        private SlotController _objectUnderTest;
+        private ISlotRepository _slotRepository;
+        private ISlotService _slotService;        
+        private IUrlHelper _urlHelper;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _slotRepository = MockRepository.GenerateStub<ISlotRepository>();        
+            _slotService = new SlotService(_slotRepository);
+            _urlHelper = MockRepository.GenerateStub<IUrlHelper>();
+            _objectUnderTest = new SlotController(_slotService, _urlHelper);
+
+            _objectUnderTest.Request = new HttpRequestMessage();
+            _objectUnderTest.Request.SetConfiguration(new HttpConfiguration());
+        }
+       
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+        }
+
+        [TestMethod]
+        public void GetLisOfSlotPages_returnAllSlotPages()
+        {
+            List<SlotPageSummary> list = new List<SlotPageSummary>()
+            {
+                {new SlotPageSummary(){ Id=1, PageName="test1"}},
+                {new SlotPageSummary(){ Id=2, PageName="test2"}}
+            };
+            _slotRepository.Stub(r => r.GetListOfSlotPages()).Return(list);
+
+            var result = _objectUnderTest.GetLisOfSlotPages();
+
+            Assert.IsNotNull(result);
+        }
+
+
+
+        [TestMethod]
+        public void GetSlotPageById_ReturnsASlotPageWithSlots()
+        {
+            SlotPage sp = new SlotPage() { Id = 1, PageName = "Test", Slots = { new Slot(){ Id=1, Headline="test slot"}  } };
+            _slotRepository.Stub(r => r.GetSlotPageById(Arg<int>.Is.Anything)).Return(sp);
+            
+            var result = _objectUnderTest.GetSlotPageById(1);
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void GetSlotPageById_WithSlotHavingAnImage_ReturnedSlotHasImageUrl()
+        {
+            //Arrange
+            HttpConfiguration config = new HttpConfiguration();
+            var route = config.Routes.MapHttpRoute
+            (
+               name: "GetMediaContentById",
+               routeTemplate: "mediacontent/{id}"
+            );
+
+            var routeData = new HttpRouteData(route, new HttpRouteValueDictionary { { "id", "1" } });
+
+            
+            HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Get, "http://localhost:80/slotpage/1");
+            req.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, config);
+            req.Properties.Add(HttpPropertyKeys.HttpRouteDataKey, routeData);
+
+            _urlHelper = new TWG.EASDataService.Api.Helpers.UrlHelper(new System.Web.Http.Routing.UrlHelper(req));
+            _objectUnderTest = new SlotController(_slotService, _urlHelper);
+            _objectUnderTest.Request = req;
+
+            var slot = new Slot() { Id = 1, Headline = "test slot", Image = new Image() { Name = "dummy image", Id = 1000, Extension = "jpg" } };
+            SlotPage sp = new SlotPage() { Id = 1, PageName = "Test", Slots = { slot } };
+            _slotRepository.Stub(r => r.GetSlotPageById(Arg<int>.Is.Anything)).Return(sp);
+
+            //Act
+            var result = _objectUnderTest.GetSlotPageById(1);
+
+            //Assert
+            Image img = result.Slots.FirstOrDefault().Image;
+            Assert.AreNotEqual(string.Empty,img.Url);
+            
+        }
+        
+    }
+}
---
+++ TWG.EASDataService.Api.Tests/StaticPageControllerTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+
+using TWG.EASDataService.Business;
+using TWG.EASDataService.Data.Repository;
+using TWG.EASDataService.Services;
+using TWG.EASDataService.Api.Controllers;
+using TWG.EASDataService.Api.Models;
+using TWG.EASDataService.Api.Helpers;
+
+namespace TWG.EASDataService.Api.Tests
+{
+    [TestClass]
+    public class StaticPageControllerTests
+    {
+        private StaticPageController _objectUnderTest;
+        private IStaticPageRepository _staticPageRepository;
+        private IStaticPageService _staticPageService;
+        private IStaticContentLinkService _staticContentLinkService;
+        private IUrlHelper _urlHelper;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _staticPageRepository = MockRepository.GenerateStub<IStaticPageRepository>();
+            _urlHelper = MockRepository.GenerateStub<IUrlHelper>();
+            _staticContentLinkService = MockRepository.GenerateStub<IStaticContentLinkService>();
+            _staticPageService = new StaticPageService(_staticPageRepository);
+            _objectUnderTest = new StaticPageController(_staticPageService, _staticContentLinkService, _urlHelper);
+
+            _objectUnderTest.Request = new HttpRequestMessage();
+            _objectUnderTest.Request.SetConfiguration(new HttpConfiguration());
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+        }
+
+        [TestMethod]
+        public void GetAllStaticPages_ReturnsAListofStaticPages()
+        {
+            List<StaticPageSummary> list = new List<StaticPageSummary>()
+            {
+                {new StaticPageSummary(){ Id=1, PageName="test1"}},
+                {new StaticPageSummary(){ Id=2, PageName="test2"}}
+            };
+            _staticPageRepository.Stub(r => r.GetAll()).Return(list);
+
+            var staticPages = _objectUnderTest.GetAllStaticPages();
+
+            Assert.IsNotNull(staticPages);
+        }
+
+
+        [TestMethod]
+        public void Get_RequestExistingStaticPage()
+        {
+            _staticPageRepository.Stub(r => r.Get(Arg<int>.Is.Anything)).Return(new StaticPage());
+            _urlHelper.Stub(h => h.GenerateUrl(Arg<String>.Is.Anything, Arg<Object>.Is.Anything)).Return("http://dummylink.co.uk");
+            _staticContentLinkService.Stub(s => s.GetStaticContentLinkForSite()).Return(new List<StaticContentLink>());
+
+            var staticPageFeed = _objectUnderTest.GetById(100);
+
+            Assert.IsNotNull(staticPageFeed);
+        }
+
+        [TestMethod]
+        public void Get_RequestNonExistingStaticpage()
+        {
+            _staticPageRepository.Stub(r => r.Get(Arg<int>.Is.Anything)).Return(null);
+            _urlHelper.Stub(h => h.GenerateUrl(Arg<String>.Is.Anything, Arg<Object>.Is.Anything)).Return("http://dummylink.co.uk");
+            _staticContentLinkService.Stub(s => s.GetStaticContentLinkForSite()).Return(new List<StaticContentLink>());
+
+            var staticPageFeed = _objectUnderTest.GetById(-1);
+
+            Assert.IsNull(staticPageFeed);
+        }
+    }
+}
